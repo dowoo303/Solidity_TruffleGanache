dc0df258b991dd9e930bc0e2aed57845dcef9517
dowoo303 <dowoo303@naver.com>
2023-06-13 17:29:20 +0900

    DAY56

---
+++ 0613/contracts/A.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract A {
+  constructor(uint _a) {
+    a = _a;
+
+  }
+  uint public a;
+
+  function setA(uint _a) public {
+    a = _a;
+  }
+
+}
+
+
+contract B {
+  uint public a;
+  
+  function setA(uint _a) public {
+    a = _a;
+  }
+
+}
